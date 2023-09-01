1aa10e51ec03823c6db462602443b8e581ec1538
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-09-01 19:23:52 +0300

    V1.0

--- mean_cal_app.py
+++ mean_cal_app.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import streamlit as st
 
+df = None
+
 def calculate_weighted_mean(group):
     valid_rows = group.dropna(subset=['finish', 'weight'], how='all')
     total_weight = valid_rows['weight'].sum()
@@ -10,6 +12,27 @@ def calculate_weighted_mean(group):
         return None
     return weighted_scores_sum / total_weight
 
+def edit_existing_course(df):
+    st.write("### Edit Existing Course")
+    course_list = df['course_name'].unique().tolist()
+    selected_course = st.selectbox("Select a course to edit:", course_list)
+    
+    # Get the current values for the selected course
+    selected_row = df[df['course_name'] == selected_course].iloc[0]
+    current_weight = selected_row['weight']
+    current_finish_score = selected_row['finish']
+    
+    # Input new values
+    new_weight = st.number_input(f"Edit Weight for {selected_course} (Current: {current_weight}):", min_value=0.0, max_value=10.0, step=0.1)
+    new_finish_score = st.number_input(f"Edit Finish Score for {selected_course} (Current: {current_finish_score}):", min_value=0, max_value=100, step=1)
+    
+    if st.button(f"Update {selected_course}"):
+        df.loc[df['course_name'] == selected_course, 'weight'] = new_weight
+        df.loc[df['course_name'] == selected_course, 'finish'] = new_finish_score
+        st.write(f"{selected_course} has been updated successfully!")
+        return df
+    return None
+
 def calculate_overall_weighted_mean(data):
     valid_rows = data.dropna(subset=['finish', 'weight'], how='all')
     total_weight = valid_rows['weight'].sum()
@@ -17,49 +40,167 @@ def calculate_overall_weighted_mean(data):
     if total_weight == 0:
         return None
     return weighted_scores_sum / total_weight
+# New Functions
+# Function to Add New Courses
+def add_new_course(df):
+    st.write("### Add a New Course")
+    course_name = st.text_input("Course Name:")
+    semester = st.radio("Semester:", ('a', 'b'))
+    course_type = st.radio("Type:", ('ba', 'bsc'))
+    year = st.radio("Year:", (1, 2, 3, 4))
+    weight = st.number_input("Weight:", min_value=0.0, max_value=10.0, step=0.1)
+    finish_score = st.number_input("Finish Score:", min_value=0, max_value=100, step=1)
+
+    if st.button("Add Course"):
+        new_row = {
+            'finish': finish_score,
+            'work': None,
+            'exam': None,
+            'weight': weight,
+            'semester': semester,
+            'type': course_type,
+            'year': year,
+            'course_name': course_name
+        }
+        df = df.append(new_row, ignore_index=True)
+        st.write(f"Course {course_name} has been added successfully!")
+        return df
+    return None
+# Function to Rate Course Difficulty
+def rate_course_difficulty(df):
+    st.write("### Rate Course Difficulty")
+    course_list = df['course_name'].unique().tolist()
+    selected_course = st.selectbox("Select a course to rate:", course_list)
+    difficulty = st.slider("Rate the difficulty of the course:", min_value=1, max_value=9, step=1)
+    if st.button("Rate Difficulty"):
+        df.loc[df['course_name'] == selected_course, 'difficulty'] = difficulty
+        st.write(f"Difficulty for course {selected_course} has been rated as {difficulty}!")
+        return df
+    return None
+
+# Function to Rate Course Difficulty
+def rate_course_difficulty(df):
+    st.write("### Rate Course Difficulty")
+    course_list = df['course_name'].unique().tolist()
+    selected_course = st.selectbox("Select a course to rate:", course_list)
+    difficulty = st.slider("Rate the difficulty of the course:", min_value=1, max_value=9, step=1)
+
+    if st.button("Rate Difficulty"):
+        df.loc[df['course_name'] == selected_course, 'difficulty'] = difficulty
+        st.write(f"Difficulty for course {selected_course} has been rated as {difficulty}!")
+        return df
+    return None
+
+# Function to Suggest Courses to Improve Final Score
+def suggest_courses_to_improve_score(df):
+    st.write("### Suggest Courses to Improve Final Score")
+    target_score = st.number_input("Target Final Score:", min_value=0, max_value=100, step=1)
+    
+    for course_type in ['ba', 'bsc']:
+        st.write(f"Suggestions for {course_type.upper()} courses:")
+        
+        # Filter the DataFrame to include only the relevant course_type
+        filtered_df = df[df['type'] == course_type]
+        
+        if filtered_df.empty:
+            st.write(f"No courses found for type {course_type.upper()}.")
+            continue
+
+        # Compute the overall weighted mean for the filtered DataFrame
+        overall_weighted_mean = calculate_overall_weighted_mean(filtered_df)
+        st.write(f"Current weighted mean for {course_type.upper()}: {overall_weighted_mean if overall_weighted_mean is not None else 'N/A'}")
+        
+        if overall_weighted_mean is None:
+            st.write("Could not compute overall weighted mean.")
+            continue
+
+        # Filter out courses that are already highly scored
+        potential_courses = filtered_df[filtered_df['finish'] < target_score].copy()
+        
+        if potential_courses.empty:
+            st.write(f"No potential courses to improve for type {course_type.upper()}.")
+            continue
+        
+        # Create 'difficulty' column if it doesn't exist
+        if 'difficulty' not in potential_courses.columns:
+            potential_courses['difficulty'] = float('nan')
+        
+        # Fill NaNs with maximum difficulty
+        potential_courses['difficulty'].fillna(9, inplace=True)
+        
+        # Calculate the impact of each course on the final score
+        potential_courses['impact'] = potential_courses['weight'] / filtered_df['weight'].sum()
+        
+        # Calculate impact per unit difficulty
+        potential_courses['impact_per_difficulty'] = potential_courses['impact'] / potential_courses['difficulty']
+        
+        # Sort courses by their impact per unit difficulty
+        suggested_courses = potential_courses.sort_values('impact_per_difficulty', ascending=False)
+        
+        # Show the top 5 suggested courses
+        st.write("#### Top 5 Suggested Courses to Help Improve Your Final Score:")
+        st.table(suggested_courses.head(5)[['course_name', 'impact', 'difficulty', 'impact_per_difficulty']])
+
+st.title("University Course Manager")
 
 # Sidebar with Help and Downloadable CSV Template
-st.sidebar.title("Help and Template")
-st.sidebar.write("Download the CSV template to see how to fill in your course data.")
-template_csv = "finish,work,exam,weight,semester,type,year,course_name"
-st.sidebar.download_button("Download CSV Template", template_csv, "course_template.csv")
+with st.sidebar:
+    st.title("Help and Template")
+    st.write("Download the CSV template to see how to fill in your course data.")
+    template_csv = "finish,work,exam,weight,semester,type,year,course_name"
+    st.download_button("Download CSV Template", template_csv, "course_template.csv")
 
+# Download Updated CSV Button
+with st.expander("Download Updated CSV"):
+    def download_link(object_to_download, download_filename, download_link_text):
+        import base64
+        if isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+        return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+    if st.button('Download Updated CSV File'):
+        tmp_download_link = download_link(df, 'updated_courses.csv', 'Click here to download your updated CSV file')
+        st.markdown(tmp_download_link, unsafe_allow_html=True)
+    if df is not None:
+        if st.button('Download Updated CSV File'):
+            tmp_download_link = download_link(df, 'updated_courses.csv', 'Click here to download your updated CSV file')
+            st.markdown(tmp_download_link, unsafe_allow_html=True)
 # File Upload
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     df.rename(columns={'simester': 'semester', 'name': 'course_name'}, inplace=True)
-    
-    # Select multiple courses to retake
-    st.write("### Want to retake some courses?")
-    course_list = df['course_name'].unique().tolist()
-    selected_courses = st.multiselect("Select the courses you'd like to retake:", course_list)
-    
-    # Create a dictionary to store new scores
-    new_scores = {}
-    for course in selected_courses:
-        new_scores[course] = st.slider(f"New score for {course}:", min_value=0, max_value=100, step=1)
-    
-    # Take a snapshot of the original data for comparison
-    original_ba_data = df[df['type'] == 'ba'].copy()
-    original_bsc_data = df[df['type'] == 'bsc'].copy()
-    
-    # Update the data with the new scores for the selected courses
-    for course, score in new_scores.items():
-        df.loc[df['course_name'] == course, 'finish'] = score
-    
-    # Group data and calculate weighted mean
-    ba_data = df[df['type'] == 'ba'].copy()
-    bsc_data = df[df['type'] == 'bsc'].copy()
-    
-    # Calculate the overall and yearly weighted means before and after retaking courses
-    overall_ba_mean_before = calculate_overall_weighted_mean(original_ba_data)
-    overall_bsc_mean_before = calculate_overall_weighted_mean(original_bsc_data)
-    overall_ba_mean_after = calculate_overall_weighted_mean(ba_data)
-    overall_bsc_mean_after = calculate_overall_weighted_mean(bsc_data)
-    
-    # Display the before and after overall weighted means
-    st.write(f"### Overall Weighted Mean for B.A. Courses: Before {overall_ba_mean_before:.2f}, After {overall_ba_mean_after:.2f}")
-    st.write(f"### Overall Weighted Mean for B.Sc. Courses: Before {overall_bsc_mean_before:.2f}, After {overall_bsc_mean_after:.2f}")
+
+    if 'difficulty' not in df.columns:
+        df['difficulty'] = float('nan')
+
+    # Call the new functions
+    new_df = add_new_course(df)
+    if new_df is not None:
+        df = new_df
+
+    new_df = edit_existing_course(df)
+    if new_df is not None:
+        df = new_df
+
+    new_df = rate_course_difficulty(df)
+    if new_df is not None:
+        df = new_df
+
+    suggest_courses_to_improve_score(df)
 else:
     st.warning("You need to upload a CSV or Excel file.")
+
+with st.expander("Download Updated CSV"):
+    def download_link(object_to_download, download_filename, download_link_text):
+        import base64
+        if isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+        return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+        
+    if df is not None:
+        if st.button('Download Updated CSV File', key='download_csv1'):
+            tmp_download_link = download_link(df, 'updated_courses.csv', 'Click here to download your updated CSV file')
+            st.markdown(tmp_download_link, unsafe_allow_html=True)
